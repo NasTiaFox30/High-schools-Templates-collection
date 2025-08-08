f084aa05adaa0cca2749fa68e8233a3764aec15a
Nastia Bzova <bzovanastia@gmail.com>
2025-08-08 21:54:03 +0200

    Feat: created universal function for PARALAX effect

--- scripts/paralax.js
+++ scripts/paralax.js
@@ -1,21 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const welcomeSection = document.getElementById('powitanie');
-    const bg = welcomeSection.querySelector('::before');
-    const bg_decor = welcomeSection.querySelector('::after');
-    const txt = document.getElementById('powitanie_content');
-  
+function paralaxEffect(elem) {
+  const paralaxSection = document.getElementById(elem);
+  const bg = paralaxSection.querySelector('::before');
+  const bg_decor = paralaxSection.querySelector('::after');
+  const content = document.getElementById(elem +'_content');
+
   window.addEventListener('scroll', function() {
     const scrollPosition = window.pageYOffset;
-    welcomeSection.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
+    paralaxSection.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
     if (bg) {
       bg.style.transform = 'translateZ(-1px) scale(2) translateY(' + scrollPosition * 0.2 + 'px)';
-      }
+    }
     if (bg_decor) {
       bg_decor.style.transform = 'translateZ(1px) scale(2) translateY(' + scrollPosition * 1 + 'px)';
-      }
-    if (txt) {
-      txt.style.transform = 'translateY(' + scrollPosition * 0.3 + 'px)';
-      }
-    
+    }
+    if (content) {
+      content.style.transform = 'translateY(' + scrollPosition * 0.3 + 'px)';
+    }
   });
-});
+}
